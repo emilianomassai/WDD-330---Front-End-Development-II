9d061ead8b8cef8e844c66e577deefd1da6e596b
emilianomassai <46691294+emilianomassai@users.noreply.github.com>
2022-11-15 22:05:53 +0000

    .

--- weeks/weatherApp/model.js
+++ weeks/weatherApp/model.js
@@ -21,11 +21,22 @@ async function getCoordinates() {
     var coordinatesLink = localStorage.getItem("CoordinatesLink")
     const response = await fetch(coordinatesLink);
     const data = await response.json();
-    // console.log(data);
-    // localStorage.setItem("lat", data.lat);
-    // localStorage.setItem("json", data);
-
     localStorage.setItem("latitude", data[0].lat);
     localStorage.setItem("longitude", data[0].lon);
 }
 
+
+
+// not working yet
+async function getWeather() {
+    const apyKey = "bedecaf711a6770c6a01a7e84725bfaf"
+    var latitude = localStorage.getItem("latitude")
+    var longitude = localStorage.getItem("longitude")
+
+
+    var weatherLink = "https://api.openweathermap.org/data/3.0/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + apyKey;
+    const response = await fetch(weatherLink);
+    const data = await response.json();
+    localStorage.setItem("json", data);
+
+}
