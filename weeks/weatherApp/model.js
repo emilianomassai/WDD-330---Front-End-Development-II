5b9aa54d7f914d651c7d2852d2b312810a25f95e
emilianomassai <46691294+emilianomassai@users.noreply.github.com>
2022-11-15 21:48:20 +0000

    coordinates working

--- weeks/weatherApp/model.js
+++ weeks/weatherApp/model.js
@@ -1,10 +1,28 @@
 
-// // Called on form's `onsubmit`
-// function submitForm() {
-//     // Getting the value of your city input
-//     var cityId = document.getElementById("cityId").value;
-//     console.log('City: ' + cityId)
-//     // Storing the value above into localStorage
-//     // localStorage.setItem("cityId", cityId);
-//     // return true;
-// }
+// take the user input and store it in the local storage
+function saveInfo() {
+
+
+    var cityId = document.getElementById("cityId").value;
+    var countryId = document.getElementById("countryId").value;
+    var stateId = document.getElementById("stateId").value;
+    const apyKey = "bedecaf711a6770c6a01a7e84725bfaf"
+    var link = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityId + "," + stateId + "," + countryId + "&appid=" + apyKey;
+    localStorage.setItem("CityName", cityId);
+    localStorage.setItem("CountryName", countryId);
+    localStorage.setItem("StateName", stateId);
+    localStorage.setItem("CoordinatesLink", link)
+
+}
+
+
+
+async function getCoordinates() {
+    var coordinatesLink = localStorage.getItem("CoordinatesLink")
+    const response = await fetch(coordinatesLink);
+    const data = await response.json();
+    console.log(data);
+    localStorage.setItem("lat", data.lat);
+    localStorage.setItem("json", data);
+}
+
